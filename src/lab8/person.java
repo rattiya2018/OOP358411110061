46ea378a09155ce78e037868febcf12c675adeb3
rattiya2018 <rungkotchasit@gmail.com>
2019-02-18 13:59:41 +0700

    Lab8

--- src/lab8/person.java
+++ src/lab8/person.java
@@ -5,18 +5,32 @@ public class person {
     //class properties
     private String  pID;
     private  String name;
+    private  String yearBorn;
 
     //constructor
 
 
+    public person(String pID, String name, String yearBorn) {
+        this.pID = pID;
+        this.name = name;
+        this.yearBorn = yearBorn;
+    }
+
     public person(String pID, String name) {
         this.pID = pID;
         this.name = name;
+
+    }
+
+
+
+    public  void introduce(){
+        System.out.println("My name is" +this.name+",");
+        System.out.println("I was born"+this.yearBorn+",");
+
     }
 
     //toString
-
-
     @Override
     public String toString() {
         return "person{" +
@@ -29,6 +43,7 @@ public class person {
         return pID;
     }
 
+
     public void setpID(String pID) {
         this.pID = pID;
     }
@@ -39,5 +54,13 @@ public class person {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getYearBorn() {
+        return yearBorn;
+    }
+
+    public void setYearBorn(String yearBorn) {
+        this.yearBorn = yearBorn;
     }
 }//class
