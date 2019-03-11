c7d6e882bff2e2d5d1dfe6fa7c4106f2d109b310
rattiya2018 <rung2639>
2019-03-11 18:22:07 +0700

    Lab1

---
+++ src/Lab1/intro.java
@@ -0,0 +1,16 @@
+package Lab1;
+
+public class intro {
+    public static void main(String[] args) {
+        System.out.println("panatda sriphanoen");
+        System.out.println("panatda09102539@gmail.com");
+        System.out.println("MT");
+        System.out.println("RMUTSV");
+
+
+
+
+
+    }//main
+
+}
